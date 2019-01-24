b687b124b99e86802a7cb0f20f8fce36983830b3
13293871521@163.com <13293871521@163.com>
2019-01-24 17:29:07 +0800

    eureka集群配置和发现!

---
+++ cloud-eureka-8762/src/main/java/com/bbd/pm/eureka/EurekaServerApplication_8762.java
@@ -0,0 +1,19 @@
+package com.bbd.pm.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description:
+ * @Date: 2019/1/17 16:40
+ * @Auther: lynn
+ */
+@SpringBootApplication
+@EnableEurekaServer //EurekaServer服务端启动类，接受其它微服务注册进来
+public class EurekaServerApplication_8762 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication_8762.class);
+        System.out.println();
+    }
+}
