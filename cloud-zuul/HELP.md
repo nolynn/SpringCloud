e2306a14f15379a927d4bb324fd09ed476248225
zhangk <13293871521@163.com>
2019-05-05 11:43:13 +0800

    push!

--- cloud-zuul/HELP.md
+++ cloud-zuul/HELP.md
@@ -7,3 +7,9 @@
     4. 打开网址访问 http://localhost:8769/api-a/hi?name=forezp 请求到了用户微服务上的接口
     4. 打开网址访问 http://localhost:8769/api-b/hi?name=forezp 请求到了组织微服务上的接口
 
+### 改造zuul zuul不仅只是路由，并且还能过滤，做一些安全验证。继续改造工程；
+    1.保持启动，创建filter 继承 ZuulFilter 并重写方法
+    2.拦截请求头做一些参数的校验
+    3.访问链接测试 
+                 http://localhost:8769/api-a/hi?name=forezp&token=22
+                 http://localhost:8769/api-b/hi?name=forezp&token=22
---
+++ cloud-zuul/src/main/java/com/lynn/zk/zuul/MyFilter.java
@@ -0,0 +1,56 @@
+package com.lynn.zk.zuul;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Description:
+ * @Date: 2019/5/5 11:38
+ * @Auther: lynn
+ */
+@Component
+public class MyFilter extends ZuulFilter {
+    private static Logger logger = LoggerFactory.getLogger(MyFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        logger.info(String.format("%s >>> %s", request.getMethod(), request.getRequestURL().toString()));
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null) {
+            logger.warn("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (Exception e) {
+            }
+
+            return null;
+        }
+        logger.info("ok");
+        return null;
+    }
+}
